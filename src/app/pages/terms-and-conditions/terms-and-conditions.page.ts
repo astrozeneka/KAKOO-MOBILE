b914022de9edb78b994147232cc5cdba3916f436
Ryan <Ryan@localhost.com>
2024-11-02 19:36:34 +0300

    The 'Terms-and-conditions' page is complete

--- src/app/pages/terms-and-conditions/terms-and-conditions.page.ts
+++ src/app/pages/terms-and-conditions/terms-and-conditions.page.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonCheckbox, IonButton } from '@ionic/angular/standalone';
+import { TopbarComponent } from 'src/app/components/topbar/topbar.component';
+import { BackButtonComponent } from 'src/app/back-button/back-button.component';
 
 @Component({
   selector: 'app-terms-and-conditions',
   templateUrl: './terms-and-conditions.page.html',
   styleUrls: ['./terms-and-conditions.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonCheckbox, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    TopbarComponent, BackButtonComponent
+  ]
 })
 export class TermsAndConditionsPage implements OnInit {
 
