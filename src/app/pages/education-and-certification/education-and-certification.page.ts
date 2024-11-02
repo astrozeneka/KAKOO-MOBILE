54d351fb4045a50881bcb7597fa0cdbf378a80b9
Ryan <Ryan@localhost.com>
2024-11-02 18:16:25 +0300

    The page 'education-and-certification' is complete

--- src/app/pages/education-and-certification/education-and-certification.page.ts
+++ src/app/pages/education-and-certification/education-and-certification.page.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { ExperienceCardComponent } from 'src/app/components/experience-card/experience-card.component';
+import { SectionHeadingComponent } from "../../components/section-heading/section-heading.component";
+import { TopbarComponent } from 'src/app/components/topbar/topbar.component';
+import { BackButtonComponent } from 'src/app/back-button/back-button.component';
 
 @Component({
   selector: 'app-education-and-certification',
   templateUrl: './education-and-certification.page.html',
   styleUrls: ['./education-and-certification.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    ExperienceCardComponent, SectionHeadingComponent, TopbarComponent, BackButtonComponent]
 })
 export class EducationAndCertificationPage implements OnInit {
 
