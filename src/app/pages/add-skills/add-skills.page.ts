356f419620c78baee6d0636660eb214bb1367aa8
Ryan <Ryan@localhost.com>
2024-11-02 17:55:44 +0300

    The page 'add-skills' is complete

--- src/app/pages/add-skills/add-skills.page.ts
+++ src/app/pages/add-skills/add-skills.page.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { ChipInputComponent } from 'src/app/components/chip-input/chip-input.component';
+import { TopbarComponent } from 'src/app/components/topbar/topbar.component';
+import { BackButtonComponent } from 'src/app/back-button/back-button.component';
 
 @Component({
   selector: 'app-add-skills',
   templateUrl: './add-skills.page.html',
   styleUrls: ['./add-skills.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule,
+    ChipInputComponent, TopbarComponent, BackButtonComponent
+  ]
 })
 export class AddSkillsPage implements OnInit {
+  testSkillsFormControl:FormControl = new FormControl([], []);
+  displayedErrorTestSkills = undefined;
+  testSkillsControlBlur = ()=>{}
 
   constructor() { }
 
