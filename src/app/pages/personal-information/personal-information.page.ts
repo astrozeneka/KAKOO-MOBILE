3eb6bc658aa5602b65e4c193e95e688a30359a22
Ryan <Ryan@localhost.com>
2024-11-05 20:46:59 +0300

    The phone-selector component works fine

--- src/app/pages/personal-information/personal-information.page.ts
+++ src/app/pages/personal-information/personal-information.page.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { TopbarComponent } from 'src/app/components/topbar/topbar.component';
 import { BackButtonComponent } from "../../back-button/back-button.component";
 import { ChipInputComponent } from 'src/app/components/chip-input/chip-input.component';
+import { PhoneSelectorComponent } from "../../submodules/phone-selector/phone-selector.component";
 
 @Component({
   selector: 'app-personal-information',
   templateUrl: './personal-information.page.html',
   styleUrls: ['./personal-information.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonItem, IonInput, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopbarComponent, BackButtonComponent, ChipInputComponent, ReactiveFormsModule],
+  imports: [IonLabel, IonItem, IonInput, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopbarComponent, BackButtonComponent, ChipInputComponent, ReactiveFormsModule, PhoneSelectorComponent],
   providers: [
   ]
 })
@@ -34,14 +35,30 @@ export class PersonalInformationPage implements OnInit {
   testStateControlBlur = () => {}
   testCityControlBlur = () => {}
 
+  // test phone (to be deleted later)
+  testPhoneFormControl:FormControl = new FormControl("+33123456789", [Validators.required]);
+  displayedErrorTestPhone:string|undefined = "Required"
+  testPhoneControlBlur = () => {}
 
   countryKeyAccessor = (country: any) => country.name;
 
-  constructor() { }
+  constructor(
+    private cdr:ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
     this.testFormControl.valueChanges.subscribe((value) => {
     })
+
+    this.testPhoneFormControl.valueChanges.subscribe((value) => {
+      console.log("Changed")
+      console.log(value)
+    })
+
+    // Test phone control
+    setTimeout(()=>{
+      this.testPhoneFormControl.patchValue("+33123456789")
+    }, 300)
   
 
   }
@@ -69,16 +86,22 @@ export class PersonalInformationPage implements OnInit {
     this.displayedErrorTest = 
       (this.testFormControl.errors as any).required ? "This field is required"
       : (this.testFormControl.errors as any).minLength ? "You should at least add 2 skills" : undefined;
-    this.testFormControl.setErrors({serverError: true});
+    this.testFormControl.setErrors({required: true});
     this.testFormControl.markAsTouched();
 
     // The county
+    this.testCountryFormControl.setErrors({required: true});
     this.displayedErrorTestCountry = 
     (this.testCountryFormControl.errors as any)?.required ? "This field is required"
     : undefined;
-    console.log("Test error")
-    this.testCountryFormControl.setErrors({serverError: true});
     this.testCountryFormControl.markAsTouched();
+
+    // The phone
+    this.testPhoneFormControl.setErrors({required: true});
+    this.displayedErrorTestPhone = 
+    (this.testPhoneFormControl.errors as any)?.required ? "This field is required"
+    : undefined;
+    this.testPhoneFormControl.markAsTouched();
     
   }
   
