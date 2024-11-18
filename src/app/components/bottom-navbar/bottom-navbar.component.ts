6205aa9d4b34a83fca292f547a345d35de4f24ad
Ryan <Ryan@localhost.com>
2024-11-18 19:20:11 +0300

    Add code for directed 'bottom-navbar'

--- src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonButton, IonIcon } from '@ionic/angular/standalone';
 import { I18nPipeShortened } from 'src/app/i18n.pipe';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-bottom-navbar',
@@ -12,11 +13,28 @@ import { I18nPipeShortened } from 'src/app/i18n.pipe';
 })
 export class BottomNavbarComponent  implements OnInit {
   @Input() tabName: string|null = null
+  tabSequences = ['dashboard', 'meetings', 'jobboard', 'referrals', 'more-29']
 
   constructor(
     public router: Router,
+    public navController: NavController
   ) { }
 
   ngOnInit() {}
+
+  goToTab(target:string) { // Experimental (to be updated later)
+    console.log(target, this.tabName)
+    console.log(this.tabSequences.indexOf(target), this.tabSequences.indexOf(this.tabName!))
+    if (this.tabSequences.indexOf(target) < this.tabSequences.indexOf(this.tabName || 'dashboard')) {
+      console.log("back")
+      this.navController.navigateBack(['/' + target], { animated: true, animationDirection: 'back' });
+    } else {
+      console.log("forward")
+      this.navController.navigateForward(['/' + target], { animated: true, animationDirection: 'forward' });
+    }
+
+
+    // this.navController.navigateRoot('/dashboard', { animated: true, animationDirection: 'back' });
+  }
 
 }
--- src/app/utils/bottom-navbar-target.ts
+++ src/app/utils/bottom-navbar-target.ts
@@ -1,10 +1,10 @@
 import { Router } from "@angular/router";
 
 export class BottomNavbarTarget {
-    public tabName: 'dashboard'|'meetings'|'jobboard'|'referrals'|'more'|null = null
+    public tabName: 'dashboard'|'meetings'|'jobboard'|'referrals'|'more-29'|null = null
 
     constructor(public router: Router) {
-        (['dashboard', 'meetings', 'jobboard', 'referrals', 'more']).forEach(tab => {
+        (['dashboard', 'meetings', 'jobboard', 'referrals', 'more-29']).forEach(tab => {
             if (this.router.url.includes(tab))
                 this.tabName = tab as any
         })
