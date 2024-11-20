df72b64fbe9b8169c97d9611af252deb43599e0c
Ryan <Ryan@localhost.com>
2024-11-20 15:50:13 +0300

    Add a link to the profile details in the topbar header

--- src/app/topbar-dashboard/topbar-dashboard.component.ts
+++ src/app/topbar-dashboard/topbar-dashboard.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonButton } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-topbar-dashboard',
   templateUrl: './topbar-dashboard.component.html',
   styleUrls: ['./topbar-dashboard.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [IonButton, RouterModule]
 })
 export class TopbarDashboardComponent  implements OnInit {
 
