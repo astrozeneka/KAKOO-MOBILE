e1c9fe7e6f5e67b20e919fbe3c5fb71a937b2034
Ryan <Ryan@localhost.com>
2024-11-23 17:58:30 +0300

    Add the placeholder page for assessments and video-assessments

--- src/app/pages/video-assessments/video-assessments.page.ts
+++ src/app/pages/video-assessments/video-assessments.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { TopbarComponent } from 'src/app/components/topbar/topbar.component';
+import { BackButtonComponent } from 'src/app/back-button/back-button.component';
+import { I18nPipeShortened } from 'src/app/i18n.pipe';
+import { Browser } from '@capacitor/browser';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-video-assessments',
   templateUrl: './video-assessments.page.html',
   styleUrls: ['./video-assessments.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopbarComponent, BackButtonComponent, I18nPipeShortened]
 })
 export class VideoAssessmentsPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openBrowser() {
+    Browser.open({url: `https://${environment.serverHost}/candidate/login`})
   }
 
 }
